8c84c36e570931d25502f2d6a099a4fba4d46e48
Gaspare <Gas9220@users.noreply.github.com>
2023-05-22 18:04:06 +0200

    Types and Projects link on header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,6 +44,12 @@
                         <li class="nav-item">
                             <a class="nav-link text-white fw-medium" href="{{url('/') }}">{{ __('Home') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white fw-medium" href="{{ route('admin.types.index') }}">{{ __('Types') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white fw-medium" href="{{ route('admin.projects.index') }}">{{ __('Projects') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
