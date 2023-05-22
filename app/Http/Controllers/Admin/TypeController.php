f8286456f3fc6217790f82af8e7c3a5841405dfe
Gaspare <Gas9220@users.noreply.github.com>
2023-05-22 17:11:45 +0200

    Delete operation on types

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -83,6 +83,8 @@ class TypeController extends Controller
      */
     public function destroy(Type $type)
     {
+        $type->delete();
 
+        return redirect()->route('admin.types.index')->with('message', "Tipo di progetto cancellato con successo");
     }
 }
