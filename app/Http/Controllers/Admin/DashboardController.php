302ca6d2e1e010e2a25800d1a8287cd80d2e5386
Gaspare <Gas9220@users.noreply.github.com>
2023-05-22 18:16:53 +0200

    Dashboard logic improvements

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,15 +7,23 @@ use App\Models\Project;
 use App\Models\Type;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        $totalProjects = DB::table('projects')->count();
-        $lastCreation = DB::table('projects')->latest()->first();
-        $lastEdit = DB::table('projects')->where('updated_at', Project::max('updated_at'))->first();
-        $totalRevenues = DB::table('projects')->sum('revenues');
-        $types = Type::all();
-        return view('admin.dashboard', compact('totalProjects', 'lastCreation', 'lastEdit', 'totalRevenues', 'types'));
+    public function index()
+    {
+        if (Schema::hasTable('projects') && Schema::hasTable('types')) {
+            $totalProjects = DB::table('projects')->count();
+            $lastCreation = DB::table('projects')->latest()->first();
+            $lastEdit = DB::table('projects')->where('updated_at', Project::max('updated_at'))->first();
+            $totalRevenues = DB::table('projects')->sum('revenues');
+            $types = Type::all();
+
+            return view('admin.dashboard', compact('totalProjects', 'lastCreation', 'lastEdit', 'totalRevenues', 'types'));
+        } else {
+        // Se faccio il login senza effettuare prima la migrazione non riceverò più l'errore causato della mancanza delle variabili che passo alla view
+            return ('Non hai creato le tabelle projects e types. Esegui le migrazioni!');
+        }
     }
 }
